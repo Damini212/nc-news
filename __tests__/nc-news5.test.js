a40931ecc8bb4527a1b8b73520140800aac1f3df
Amrutha <a.chilumuri@gmail.com>
2023-06-27 12:25:01 +0100

    sorted using jest in the test and have added a test to check the body property

--- __tests__/nc-news5.test.js
+++ __tests__/nc-news5.test.js
@@ -41,33 +41,25 @@ describe("/api/articles", () => {
         });
       });
   });
-  test("200 should return the dates created in descending order", () => {
+  test("200 should return the sorted dates in descending order", () => {
     return request(app)
       .get("/api/articles")
       .expect(200)
       .then(({ body }) => {
         const { articles } = body;
-        expect(articles[0]).toEqual({
-          article_id: 34,
-          article_img_url:
-            "https://images.pexels.com/photos/2403392/pexels-photo-2403392.jpeg?w=700&h=700",
-          author: "grumpy19",
-          comment_count: "11",
-          created_at: "2020-11-22T11:13:00.000Z",
-          title: "The Notorious MSG’s Unlikely Formula For Success",
-          topic: "cooking",
-          votes: 0,
+        expect(articles).toBeSortedBy("created_at", {
+          descending: true,
         });
-        expect(articles[1]).toEqual({
-          article_id: 12,
-          article_img_url:
-            "https://images.pexels.com/photos/10845119/pexels-photo-10845119.jpeg?w=700&h=700",
-          author: "tickle122",
-          comment_count: "7",
-          created_at: "2020-11-15T13:25:00.000Z",
-          title: "The battle for Node.js security has only begun",
-          topic: "coding",
-          votes: 0,
+      });
+  });
+  test("200 should return a array without the body property", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        const { articles } = body;
+        articles.forEach((article) => {
+          expect(article).not.toHaveProperty("body");
         });
       });
   });
