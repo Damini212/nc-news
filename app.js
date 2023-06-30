60efa9f48496162dc30e5ddff677d6ffe91eedc6
Amrutha <a.chilumuri@gmail.com>
2023-06-30 14:55:31 +0100

    added the /404 endpoint

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ app.post("/api/articles/:article_id", patchArticleId);
 app.patch("/api/articles/:article_id", patchArticleId);
 app.delete("/api/comments/:comment_id", deleteComment);
 app.get("/api/users", getUsers);
+app.all("*", (_, res) => {
+  res.status(404).send({ status: 404, message: "Not found" });
+});
 
 app.use(handleCustomErrors);
 app.use(handlePsqlErrors);
